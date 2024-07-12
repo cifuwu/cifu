21cb1eb6e7a3b6f8315e7afbd34004a9e4c142c2
cifuwu <sebastian.cifuentesm@sansano.usm.cl>
2024-07-12 19:29:50 -0400

    icon

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Mensaje para ti",
-  description: "Troliao jajajajaj",
+  title: "Caca",
+  description: "Mensaje para ti",
 };
 
 export default function RootLayout({ children }) {
