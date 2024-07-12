a5e7c62159d2a5dfe80e2bd09be9a0bba59c1c19
cifuwu <sebastian.cifuentesm@sansano.usm.cl>
2024-07-12 19:35:05 -0400

    margenes

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import animacion from "@/public/animacion.json";
 export default function Home() {
   return (
     <main className="flex items-center justify-center h-screen w-screen bg-gray-50">
-      <div className="w-full sm:max-w-2xl -mt-40 mx-5">
+      <div className="w-full sm:max-w-2xl -mt-24 md:-mt-40 mx-5">
         <Lottie animationData={animacion} loop={true} />
         <h2 className="text-center text-2xl md:text-4xl font-extrabold text-gray-900">Voy corriendo a pegarte unos combos</h2>
       </div>
