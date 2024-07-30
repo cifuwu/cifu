5248f02b73179d94eaa12ae327b8da17e7b222a3
cifuwu <sebastian.cifuentesm@sansano.usm.cl>
2024-07-30 04:09:42 -0400

    analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <body className={`${inter.className} bg-gray-50`}>{children}</body>
+      <GoogleAnalytics gaId="G-131L8RY4Y4" />
     </html>
   );
 }
